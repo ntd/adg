cbbecdf99c39ecdad38c777609a9bf8bb7ea36b3
Nicola Fontana <ntd@entidi.it>
2008-06-29 16:48:54 +0000

    Added forgotten AdgArrowStyle files

---
+++ adg/adg-arrow-style.c
@@ -0,0 +1,335 @@
+/* ADG - Automatic Drawing Generation
+ * Copyright (C) 2007-2008, Nicola Fontana <ntd at entidi.it>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the 
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA  02111-1307, USA.
+ */
+
+
+/**
+ * SECTION:adgarrowstyle
+ * @title: AdgArrowStyle
+ * @short_description: arrow rendering related stuff
+ *
+ * Contains parameters on how to draw arrows, providing a way to register a
+ * custom rendering callback.
+ */
+
+#include "adg-arrow-style.h"
+#include "adg-arrow-style-private.h"
+#include "adg-intl.h"
+#include "adg-util.h"
+
+#include <math.h>
+#include <cpml.h>
+
+#define PARENT_CLASS ((AdgStyleClass *) adg_arrow_style_parent_class)
+
+
+enum
+{
+  PROP_0,
+  PROP_SIZE,
+  PROP_ANGLE,
+  PROP_MARGIN,
+  PROP_RENDERER
+};
+
+
+static void	get_property		(GObject	*object,
+					 guint		 prop_id,
+					 GValue		*value,
+					 GParamSpec	*pspec);
+static void	set_property		(GObject	*object,
+					 guint		 prop_id,
+					 const GValue	*value,
+					 GParamSpec	*pspec);
+static void	arrow_renderer		(AdgArrowStyle	*arrow_style,
+					 cairo_t	*cr,
+					 cairo_path_t	*path,
+					 double		 position);
+static void	triangle_renderer	(AdgArrowStyle	*arrow_style,
+					 cairo_t	*cr,
+					 cairo_path_t	*path,
+					 double		 position);
+static void	dot_renderer		(AdgArrowStyle	*arrow_style,
+					 cairo_t	*cr,
+					 cairo_path_t	*path,
+					 double		 position);
+static void	circle_renderer		(AdgArrowStyle	*arrow_style,
+					 cairo_t	*cr,
+					 cairo_path_t	*path,
+					 double		 position);
+static void	block_renderer		(AdgArrowStyle	*arrow_style,
+					 cairo_t	*cr,
+					 cairo_path_t	*path,
+					 double		 position);
+static void	square_renderer		(AdgArrowStyle	*arrow_style,
+					 cairo_t	*cr,
+					 cairo_path_t	*path,
+					 double		 position);
+static void	tick_renderer		(AdgArrowStyle	*arrow_style,
+					 cairo_t	*cr,
+					 cairo_path_t	*path,
+					 double		 position);
+
+
+G_DEFINE_TYPE (AdgArrowStyle, adg_arrow_style, ADG_TYPE_STYLE)
+
+
+static void
+adg_arrow_style_class_init (AdgArrowStyleClass *klass)
+{
+  GObjectClass *gobject_class;
+  GParamSpec   *param;
+
+  gobject_class = (GObjectClass *) klass;
+
+  g_type_class_add_private (klass, sizeof (AdgArrowStylePrivate));
+
+  gobject_class->get_property = get_property;
+  gobject_class->set_property = set_property;
+
+  param = g_param_spec_double ("size",
+			       P_("Arrow Size"),
+			       P_("The size of the arrow, a renderer dependent parameter"),
+			       G_MINDOUBLE, G_MAXDOUBLE, 14.,
+			       G_PARAM_READWRITE);
+  g_object_class_install_property (gobject_class, PROP_SIZE, param);
+
+  param = g_param_spec_double ("angle",
+			       P_("Arrow Angle"),
+			       P_("The angle of the arrow, a renderer dependent parameter"),
+			       G_MINDOUBLE, G_MAXDOUBLE, G_PI / 6.,
+			       G_PARAM_READWRITE);
+  g_object_class_install_property (gobject_class, PROP_ANGLE, param);
+
+  param = g_param_spec_double ("margin",
+			       P_("Arrow Margin"),
+			       P_("The margin of the arrow, a renderer dependent parameter"),
+			       G_MINDOUBLE, G_MAXDOUBLE, 14.,
+			       G_PARAM_READWRITE);
+  g_object_class_install_property (gobject_class, PROP_MARGIN, param);
+
+  param = g_param_spec_pointer ("renderer",
+				P_("Renderer Callback"),
+				P_("The callback to call to renderer this arrow type"),
+				G_PARAM_READWRITE);
+  g_object_class_install_property (gobject_class, PROP_RENDERER, param);
+}
+
+static void
+adg_arrow_style_init (AdgArrowStyle *arrow_style)
+{
+  AdgArrowStylePrivate *priv = G_TYPE_INSTANCE_GET_PRIVATE (arrow_style,
+							    ADG_TYPE_ARROW_STYLE,
+							    AdgArrowStylePrivate);
+
+  priv->size = 14.;
+  priv->angle = G_PI / 6.;
+  priv->margin = 14.;
+  priv->renderer = NULL;
+
+  arrow_style->priv = priv;
+}
+
+static void
+get_property (GObject    *object,
+	      guint       prop_id,
+	      GValue     *value,
+	      GParamSpec *pspec)
+{
+  AdgArrowStyle *arrow_style = (AdgArrowStyle *) object;
+
+  switch (prop_id)
+    {
+    case PROP_SIZE:
+      g_value_set_double (value, arrow_style->priv->size);
+      break;
+    case PROP_ANGLE:
+      g_value_set_double (value, arrow_style->priv->angle);
+      break;
+    case PROP_MARGIN:
+      g_value_set_double (value, arrow_style->priv->margin);
+      break;
+    case PROP_RENDERER:
+      g_value_set_pointer (value, arrow_style->priv->renderer);
+      break;
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+      break;
+    }
+}
+
+static void
+set_property (GObject      *object,
+	      guint         prop_id,
+	      const GValue *value,
+	      GParamSpec   *pspec)
+{
+  AdgArrowStyle *arrow_style = (AdgArrowStyle *) object;
+
+  switch (prop_id)
+    {
+    case PROP_SIZE:
+      arrow_style->priv->size = g_value_get_double (value);
+      break;
+    case PROP_ANGLE:
+      arrow_style->priv->angle = g_value_get_double (value);
+      break;
+    case PROP_MARGIN:
+      arrow_style->priv->margin = g_value_get_double (value);
+      break;
+    case PROP_RENDERER:
+      arrow_style->priv->renderer = g_value_get_pointer (value);
+      break;
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+      break;
+    }
+}
+
+
+AdgStyle *
+adg_arrow_style_from_id (AdgArrowStyleId id)
+{
+  static AdgStyle **builtins = NULL;
+
+  if G_UNLIKELY (builtins == NULL)
+    {
+      builtins = g_new (AdgStyle *, ADG_ARROW_STYLE_LAST);
+
+      builtins[ADG_ARROW_STYLE_ARROW] = g_object_new (ADG_TYPE_ARROW_STYLE,
+						      "renderer", arrow_renderer,
+						      NULL);
+      builtins[ADG_ARROW_STYLE_TRIANGLE] = g_object_new (ADG_TYPE_ARROW_STYLE,
+							 "renderer", triangle_renderer,
+							 NULL);
+      builtins[ADG_ARROW_STYLE_DOT] = g_object_new (ADG_TYPE_ARROW_STYLE,
+						    "size", 5.,
+						    "angle", 0.,
+						    "margin", 2.5,
+						    "renderer", dot_renderer,
+						    NULL);
+      builtins[ADG_ARROW_STYLE_CIRCLE] = g_object_new (ADG_TYPE_ARROW_STYLE,
+						       "size", 10.,
+						       "angle", 0.,
+						       "margin", 5.,
+						       "renderer", circle_renderer,
+						       NULL);
+      builtins[ADG_ARROW_STYLE_BLOCK] = g_object_new (ADG_TYPE_ARROW_STYLE,
+						      "size", 10.,
+						      "angle", 0.,
+						      "margin", 5.,
+						      "renderer", block_renderer,
+						      NULL);
+      builtins[ADG_ARROW_STYLE_SQUARE] = g_object_new (ADG_TYPE_ARROW_STYLE,
+						       "size", 10.,
+						       "angle", 0.,
+						       "margin", 5.,
+						       "renderer", square_renderer,
+						       NULL);
+      builtins[ADG_ARROW_STYLE_TICK] = g_object_new (ADG_TYPE_ARROW_STYLE,
+						     "size", 20.,
+						     "angle", G_PI / 3.,
+						     "margin", 0.,
+						     "renderer", tick_renderer,
+						     NULL);
+    }
+
+  g_return_val_if_fail (id < ADG_ARROW_STYLE_LAST, NULL);
+  return builtins[id];
+}
+
+void
+adg_arrow_style_render (AdgArrowStyle *arrow_style,
+			cairo_t       *cr,
+			cairo_path_t  *path,
+			double         position)
+{
+  g_return_if_fail (arrow_style != NULL);
+  g_return_if_fail (cr != NULL);
+  g_return_if_fail (path != NULL);
+  g_return_if_fail (path->num_data > 0);
+
+  /* NULL renderer */
+  if (arrow_style->priv->renderer == NULL)
+    return;
+
+  arrow_style->priv->renderer (arrow_style, cr, path, position);
+}
+
+static void
+arrow_renderer (AdgArrowStyle *arrow_style,
+		cairo_t       *cr,
+		cairo_path_t  *path,
+		double         position)
+{
+  ADG_STUB ();
+}
+
+static void
+triangle_renderer (AdgArrowStyle *arrow_style,
+		   cairo_t       *cr,
+		   cairo_path_t  *path,
+		   double         position)
+{
+  ADG_STUB ();
+}
+
+static void
+dot_renderer (AdgArrowStyle *arrow_style,
+	      cairo_t       *cr,
+	      cairo_path_t  *path,
+	      double         position)
+{
+  ADG_STUB ();
+}
+
+static void
+circle_renderer (AdgArrowStyle *arrow_style,
+		 cairo_t       *cr,
+		 cairo_path_t  *path,
+		 double         position)
+{
+  ADG_STUB ();
+}
+
+static void
+block_renderer (AdgArrowStyle *arrow_style,
+		cairo_t       *cr,
+		cairo_path_t  *path,
+		double         position)
+{
+  ADG_STUB ();
+}
+
+static void
+square_renderer (AdgArrowStyle *arrow_style,
+		 cairo_t       *cr,
+		 cairo_path_t  *path,
+		 double         position)
+{
+  ADG_STUB ();
+}
+
+static void
+tick_renderer (AdgArrowStyle *arrow_style,
+	       cairo_t       *cr,
+	       cairo_path_t  *path,
+	       double         position)
+{
+  ADG_STUB ();
+}
---
+++ adg/adg-arrow-style.h
@@ -0,0 +1,82 @@
+/* ADG - Automatic Drawing Generation
+ * Copyright (C) 2007-2008, Nicola Fontana <ntd at entidi.it>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the 
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA  02111-1307, USA.
+ */
+
+
+#ifndef __ADG_ARROW_STYLE_H__
+#define __ADG_ARROW_STYLE_H__
+
+#include <adg/adg-style.h>
+#include <adg/adg-enums.h>
+
+
+G_BEGIN_DECLS
+
+
+#define ADG_TYPE_ARROW_STYLE             (adg_arrow_style_get_type ())
+#define ADG_ARROW_STYLE(obj)             (G_TYPE_CHECK_INSTANCE_CAST ((obj), ADG_TYPE_ARROW_STYLE, AdgArrowStyle))
+#define ADG_ARROW_STYLE_CLASS(klass)     (G_TYPE_CHECK_CLASS_CAST ((klass), ADG_TYPE_ARROW_STYLE, AdgArrowStyleClass))
+#define ADG_IS_ARROW_STYLE(obj)          (G_TYPE_CHECK_INSTANCE_TYPE ((obj), ADG_TYPE_ARROW_STYLE))
+#define ADG_IS_ARROW_STYLE_CLASS(klass)  (G_TYPE_CHECK_CLASS_TYPE ((klass), ADG_TYPE_ARROW_STYLE))
+#define ADG_ARROW_STYLE_GET_CLASS(obj)   (G_TYPE_INSTANCE_GET_CLASS ((obj), ADG_TYPE_ARROW_STYLE, AdgArrowStyleClass))
+
+/* AdgArrowStyle declared in adg-style.h */
+typedef struct _AdgArrowStyleClass   AdgArrowStyleClass;
+typedef struct _AdgArrowStylePrivate AdgArrowStylePrivate;
+
+/**
+ * AdgArrowStyleRenderer:
+ * @arrow_style: an #AdgArrowStyle object
+ * @cr: a #cairo_t drawing context
+ * @path: the path where rendering the arrow
+ * @position: position on @path (0 = start; 1 = end) where to render
+ *
+ * Callback that renders a custom arrow at @position in @path.
+ */
+typedef void (*AdgArrowStyleRenderer)		(AdgArrowStyle	*arrow_style,
+						 cairo_t	*cr,
+						 cairo_path_t	*path,
+						 double		 position);
+
+struct _AdgArrowStyle
+{
+  AdgStyle		 style;
+
+  /*< private >*/
+  AdgArrowStylePrivate	*priv;
+};
+
+struct _AdgArrowStyleClass
+{
+  AdgStyleClass		 parent_class;
+};
+
+
+GType		adg_arrow_style_get_type	(void) G_GNUC_CONST;
+AdgStyle *	adg_arrow_style_new		(void);
+AdgStyle *	adg_arrow_style_from_id		(AdgArrowStyleId id);
+void            adg_arrow_style_render		(AdgArrowStyle	*arrow_style,
+						 cairo_t	*cr,
+						 cairo_path_t	*path,
+						 double		 position);
+
+
+G_END_DECLS
+
+
+#endif /* __ADG_ARROW_STYLE_H__ */
