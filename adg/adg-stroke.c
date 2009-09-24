cc0ebe1902b29552159c79cd5e09c03d0680cf58
Nicola Fontana <ntd@entidi.it>
2009-09-24 13:33:44 +0200

    [AdgStroke] Using the new AdgEntity APIs
    Updated the code using the new API functions.

--- adg/adg-stroke.c
+++ adg/adg-stroke.c
@@ -287,11 +287,13 @@ render(AdgEntity *entity, cairo_t *cr)
 
     if (cairo_path != NULL) {
         AdgLineStyle *line_style;
+        AdgMatrix ctm;
 
         line_style = (AdgLineStyle *) adg_entity_style(entity, data->dress);
+        adg_entity_get_ctm(entity, &ctm);
 
         cairo_save(cr);
-        adg_entity_apply_local_matrix(entity, cr);
+        cairo_set_matrix(cr, &ctm);
         cairo_append_path(cr, cairo_path);
         cairo_restore(cr);
 
