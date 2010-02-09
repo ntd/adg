1e2e00aff031457f98c555c87a44cab5f5e6db21
Nicola Fontana <ntd@entidi.it>
2010-02-09 17:20:21 +0100

    [tests] Initial (not working) test
    Added support for the GLib test framework and implemented first (not
    working) test.
    
    configure.ac must be updated to make the test framework optional because
    it is based on a feature introduced by glib-2.16.0 (still too young).

---
+++ tests/properties.c
@@ -0,0 +1,69 @@
+/* ADG - Automatic Drawing Generation
+ * Copyright (C) 2007,2008,2009 Nicola Fontana <ntd at entidi.it>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301, USA.
+ */
+
+
+#include <adg/adg.h>
+#include <stdlib.h>
+
+
+typedef struct {
+    AdgPath *path;
+} FixtureAdg;
+
+
+static void
+adg_setup(FixtureAdg *fixture, gconstpointer test_data)
+{
+    g_assert(test_data == (void*) 0xfeedcafe);
+
+    fixture->path = adg_path_new();
+}
+
+static void
+adg_teardown(FixtureAdg *fixture, gconstpointer test_data)
+{
+    g_assert(test_data == (void*) 0xfeedcafe);
+
+    g_object_unref(fixture->path);
+    fixture->path = NULL;
+}
+
+
+static void
+test_model(FixtureAdg *fixture, gconstpointer test_data)
+{
+    if (g_test_trap_fork(0, G_TEST_TRAP_SILENCE_STDERR)) {
+        g_object_set(fixture->path, "dependency", NULL, NULL);
+        exit(0);
+    }
+    g_test_trap_assert_passed();
+}
+
+
+int
+main(int argc, char *argv[])
+{
+    g_test_init(&argc, &argv, NULL);
+    g_type_init();
+
+    g_test_add("/adg/AdgModel", FixtureAdg, (void*) 0xfeedcafe,
+               adg_setup, test_model, adg_teardown);
+
+    return g_test_run();
+}
