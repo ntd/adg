6bf649e4e55ed34a650da02d94838810f8c4d086
Nicola Fontana <ntd@entidi.it>
2010-02-10 22:29:29 +0100

    [CPML] Added (still unused) primitive VT
    CpmlPrimitiveClass is the abstract class to be implemented by all the
    primitive types. The next step will be to fill the virtual table of
    every primitive and collect them in some way for further processing by
    the CpmlPrimitive public APIs.
    
    The actual idea is to instantiate different CpmlPrimitiveClass, once for
    every primitive type. Once done, the CpmlPrimitive abstract methods will
    be able to access the underlying implementations from a single entry
    point. A bunch of hidden initialization functions will be needed as well
    (similar to what the g_..._get_type() functions do the GObject world).
    
    Started issue #30: http://dev.entidi.com/p/adg/issues/30/

---
+++ cpml/cpml-primitive-private.h
@@ -0,0 +1,85 @@
+/* CPML - Cairo Path Manipulation Library
+ * Copyright (C) 2008, 2009  Nicola Fontana <ntd at entidi.it>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301, USA.
+ */
+
+
+#ifndef __CPML_PRIMITIVE_PRIVATE_H__
+#define __CPML_PRIMITIVE_PRIVATE_H__
+
+
+CAIRO_BEGIN_DECLS
+
+/**
+ * CpmlPrimitiveClass:
+ * @name:              descriptive name of the primitive type. This name
+ *                     will be used for debugging purpose and while
+ *                     dumping the primitive data.
+ * @n_points:          exact number of points needed to properly define
+ *                     a primitive of this class type.
+ * @get_length:        gets the length of a primitive.
+ * @put_extents:       gets the bounding box of a primitive.
+ * @put_pair_at:       gets the coordinates of a point on the primitive at
+ *                     a given factor.
+ * @put_vector_at:     gets the vector of a point on the primitive at a
+ *                     given factor.
+ * @get_closest_pos:   gets the factor of the point on a primitive closest
+ *                     to another given point.
+ * @put_intersections: gets the intersection points between a primitive of
+ *                     this type and a primitive of any type. The number
+ *                     of returned intersections can be explicitely limited.
+ * @offset:            creates a new primitive of tha same type parallel
+ *                     to the original one with a given distance.
+ * @join:              join two primitives (the first one of this class type)
+ *                     by modifying the end point of the first one and the
+ *                     start point of the second one.
+ *
+ * Any primitive type must implement an instance of this class as a
+ * global variable. This will abstract the primitives and allows to
+ * access them throught the cpml_primitive_...() APIs.
+ */
+typedef struct _CpmlPrimitiveClass CpmlPrimitiveClass;
+
+struct _CpmlPrimitiveClass {
+    const char   *name;
+    int           n_points;
+
+    double       (*get_length)          (const CpmlPrimitive    *primitive);
+    void         (*put_extents)         (const CpmlPrimitive    *primitive,
+                                         CpmlExtents            *extents);
+    void         (*put_pair_at)         (const CpmlPrimitive    *primitive,
+                                         double                  pos,
+                                         CpmlPair               *pair);
+    void         (*put_vector_at)       (const CpmlPrimitive    *primitive,
+                                         double                  pos,
+                                         CpmlVector             *vector);
+    double       (*get_closest_pos)     (const CpmlPrimitive    *primitive,
+                                         const CpmlPair         *pair);
+    int          (*put_intersections)   (const CpmlPrimitive    *primitive,
+                                         const CpmlPrimitive    *primitive2,
+                                         int                     max,
+                                         CpmlPair               *dest);
+    void         (*offset)              (CpmlPrimitive          *primitive,
+                                         double                  offset);
+    cairo_bool_t (*join)                (CpmlPrimitive          *primitive,
+                                         CpmlPrimitive          *primitive2);
+};
+
+CAIRO_END_DECLS
+
+
+#endif /* __CPML_PRIMITIVE_PRIVATE_H__ */
