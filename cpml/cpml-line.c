9b4cade7e2dcbcca3bf4940546b2f2b195ffae1c
Nicola Fontana <ntd@entidi.it>
2010-02-12 15:39:57 +0100

    [CPML] Merged line and close implementation
    These two primitives are similar enough to justify the merging. This
    will be an advantage while populating the virtual table of the primitive
    classes, as a lot of methods will be shared.

--- cpml/cpml-line.c
+++ cpml/cpml-line.c
@@ -30,6 +30,21 @@
  * is effectively a straingt line.
  **/
 
+/**
+ * SECTION:cpml-close
+ * @Section_Id:CpmlClose
+ * @title: CpmlClose
+ * @short_description: Straigth line used to close cyclic segments
+ *
+ * The following functions manipulate %CAIRO_PATH_CLOSE_PATH
+ * #CpmlPrimitive. No validation is made on the input so use the
+ * following methods only when you are sure the
+ * <varname>primitive</varname> argument is effectively a close path.
+ *
+ * This primitive management is almost identical to straight lines,
+ * but taking properly start and end points.
+ **/
+
 
 #include "cpml-internal.h"
 #include "cpml-extents.h"
@@ -53,6 +68,29 @@ _cpml_line_get_class(void)
     if (p_class == NULL) {
         static _CpmlPrimitiveClass class_data = {
             "line", 2,
+            NULL,
+            NULL,
+            NULL,
+            NULL,
+            NULL,
+            NULL,
+            NULL,
+            NULL
+        };
+        p_class = &class_data;
+    }
+
+    return p_class;
+}
+
+const _CpmlPrimitiveClass *
+_cpml_close_get_class(void)
+{
+    static _CpmlPrimitiveClass *p_class = NULL;
+
+    if (p_class == NULL) {
+        static _CpmlPrimitiveClass class_data = {
+            "close", 2,
             NULL,
             NULL,
             NULL,
@@ -268,6 +306,74 @@ cpml_line_offset(CpmlPrimitive *line, double offset)
     p1->point.y += normal.y;
     p2->point.x += normal.x;
     p2->point.y += normal.y;
+}
+
+
+/**
+ * cpml_close_put_pair_at:
+ * @close: the #CpmlPrimitive close data
+ * @pos:  the position value
+ * @pair: the destination pair
+ *
+ * Given the @close path virtual primitive, finds the coordinates
+ * at position @pos (where 0 is the start and 1 is the end) and
+ * stores the result in @pair.
+ *
+ * @pos can be less than 0 or greater than 1, in which case the
+ * coordinates are interpolated.
+ **/
+void
+cpml_close_put_pair_at(const CpmlPrimitive *close, double pos, CpmlPair *pair)
+{
+    cpml_line_put_pair_at(close, pos, pair);
+}
+
+/**
+ * cpml_close_put_vector_at:
+ * @close:  the #CpmlPrimitive close data
+ * @vector: the destination vector
+ * @pos:    the position value
+ *
+ * Gets the slope on @close at the position @pos. Being the
+ * close a straight line, the vector is always the same, so
+ * @pos is not used.
+ **/
+void
+cpml_close_put_vector_at(const CpmlPrimitive *close, double pos,
+                         CpmlVector *vector)
+{
+    cpml_line_put_vector_at(close, pos, vector);
+}
+
+/**
+ * cpml_close_get_closest_pos:
+ * @close: the #CpmlPrimitive close data
+ * @pair: the coordinates of the subject point
+ *
+ * Returns the pos value of the point on @close nearest to @pair.
+ * The returned value is always between 0 and 1.
+ *
+ * Returns: the pos value, always between 0 and 1
+ **/
+double
+cpml_close_get_closest_pos(const CpmlPrimitive *close, const CpmlPair *pair)
+{
+    return cpml_line_get_closest_pos(close, pair);
+}
+
+/**
+ * cpml_close_offset:
+ * @close:  the #CpmlPrimitive close data
+ * @offset: distance for the computed parallel close
+ *
+ * Given a close segment specified by the @close primitive data,
+ * computes the parallel close distant @offset from the original one
+ * and returns the result by changing @close.
+ **/
+void
+cpml_close_offset(CpmlPrimitive *close, double offset)
+{
+    cpml_line_offset(close, offset);
 }
 
 
