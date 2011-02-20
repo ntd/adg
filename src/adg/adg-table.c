099e2b9f89130ff379eaf68d5d260834f30bf05e
Nicola Fontana <ntd@entidi.it>
2011-02-20 20:00:27 +0100

    [AdgTable] Added support for AdgText

--- src/adg/adg-table.c
+++ src/adg/adg-table.c
@@ -72,16 +72,15 @@
 
 
 #include "adg-internal.h"
+#include "adg-text-internal.h"
+
 #include "adg-model.h"
 #include "adg-trail.h"
 #include "adg-dress.h"
 #include "adg-dress-builtins.h"
-#include "adg-style.h"
 #include "adg-table-style.h"
 #include "adg-path.h"
 #include "adg-stroke.h"
-#include "adg-textual.h"
-#include "adg-toy-text.h"
 #include "adg-container.h"
 #include "adg-alignment.h"
 
@@ -809,7 +808,7 @@ adg_table_cell_set_text_title(AdgTableCell *cell, const gchar *title)
                                                      table_dress);
     padding = adg_table_style_get_cell_padding(table_style);
     font_dress = adg_table_style_get_title_dress(table_style);
-    entity = g_object_new(ADG_TYPE_TOY_TEXT, "text", title,
+    entity = g_object_new(ADG_TYPE_BEST_TEXT, "text", title,
                           "font-dress", font_dress, NULL);
 
     cairo_matrix_init_translate(&map, padding->x, padding->y);
@@ -903,7 +902,7 @@ adg_table_cell_set_text_value(AdgTableCell *cell, const gchar *value)
                                                      table_dress);
     padding = adg_table_style_get_cell_padding(table_style);
     font_dress = adg_table_style_get_value_dress(table_style);
-    entity = g_object_new(ADG_TYPE_TOY_TEXT, "text", value,
+    entity = g_object_new(ADG_TYPE_BEST_TEXT, "text", value,
                           "font-dress", font_dress, NULL);
 
     cairo_matrix_init_translate(&map, 0, -padding->y);
