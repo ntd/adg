c0152fc733413c9dd33c3beb20a121d46d5ad7ab
Nicola Fontana <ntd@entidi.it>
2012-03-24 01:09:22 +0100

    test-text: implemented test overriding old pasted code
    The code in test-text was copy&pasted from test-toy-text but never
    updated to use AdgText.

--- src/adg/tests/test-text.c
+++ src/adg/tests/test-text.c
@@ -24,13 +24,13 @@
 static void
 _adg_test_font_dress(void)
 {
-    AdgToyText *toy_text;
+    AdgText *text;
     AdgTextual *textual;
     AdgDress valid_dress_1, valid_dress_2, incompatible_dress;
     AdgDress font_dress;
 
-    toy_text = adg_toy_text_new(NULL);
-    textual = (AdgTextual *) toy_text;
+    text = adg_text_new(NULL);
+    textual = (AdgTextual *) text;
     valid_dress_1 = ADG_DRESS_FONT_QUOTE_ANNOTATION;
     valid_dress_2 = ADG_DRESS_FONT;
     incompatible_dress = ADG_DRESS_LINE;
@@ -49,66 +49,66 @@ _adg_test_font_dress(void)
     g_assert_cmpint(font_dress, ==, valid_dress_2);
 
     /* Using GObject property methods */
-    g_object_set(toy_text, "font-dress", valid_dress_1, NULL);
-    g_object_get(toy_text, "font-dress", &font_dress, NULL);
+    g_object_set(text, "font-dress", valid_dress_1, NULL);
+    g_object_get(text, "font-dress", &font_dress, NULL);
     g_assert_cmpint(font_dress, ==, valid_dress_1);
 
-    g_object_set(toy_text, "font-dress", incompatible_dress, NULL);
-    g_object_get(toy_text, "font-dress", &font_dress, NULL);
+    g_object_set(text, "font-dress", incompatible_dress, NULL);
+    g_object_get(text, "font-dress", &font_dress, NULL);
     g_assert_cmpint(font_dress, ==, valid_dress_1);
 
-    g_object_set(toy_text, "font-dress", valid_dress_2, NULL);
-    g_object_get(toy_text, "font-dress", &font_dress, NULL);
+    g_object_set(text, "font-dress", valid_dress_2, NULL);
+    g_object_get(text, "font-dress", &font_dress, NULL);
     g_assert_cmpint(font_dress, ==, valid_dress_2);
 
-    adg_entity_destroy(ADG_ENTITY(toy_text));
+    adg_entity_destroy(ADG_ENTITY(text));
 }
 
 static void
 _adg_test_text(void)
 {
-    AdgToyText *toy_text;
+    AdgText *text;
     AdgTextual *textual;
     const gchar *valid_text, *latin1_text;
-    gchar *text;
+    gchar *string;
 
-    toy_text = adg_toy_text_new(NULL);
-    textual = (AdgTextual *) toy_text;
-    valid_text = "This is some text...";
-    latin1_text = "This is some àèìòù Latin1 text...";
+    text = adg_text_new(NULL);
+    textual = (AdgTextual *) text;
+    valid_text = "This is some string...";
+    latin1_text = "This is some àèìòù Latin1 string...";
 
     /* Using the public APIs */
     adg_textual_set_text(textual, valid_text);
-    text = adg_textual_dup_text(textual);
-    g_assert_cmpstr(text, ==, valid_text);
-    g_free(text);
+    string = adg_textual_dup_text(textual);
+    g_assert_cmpstr(string, ==, valid_text);
+    g_free(string);
 
     adg_textual_set_text(textual, latin1_text);
-    text = adg_textual_dup_text(textual);
-    g_assert_cmpstr(text, ==, latin1_text);
-    g_free(text);
+    string = adg_textual_dup_text(textual);
+    g_assert_cmpstr(string, ==, latin1_text);
+    g_free(string);
 
     adg_textual_set_text(textual, NULL);
-    text = adg_textual_dup_text(textual);
-    g_assert(text == NULL);
-    g_free(text);
+    string = adg_textual_dup_text(textual);
+    g_assert(string == NULL);
+    g_free(string);
 
     /* Using GObject property methods */
-    g_object_set(toy_text, "text", valid_text, NULL);
-    g_object_get(toy_text, "text", &text, NULL);
-    g_assert_cmpstr(text, ==, valid_text);
-    g_free(text);
+    g_object_set(text, "text", valid_text, NULL);
+    g_object_get(text, "text", &string, NULL);
+    g_assert_cmpstr(string, ==, valid_text);
+    g_free(string);
 
-    g_object_set(toy_text, "text", latin1_text, NULL);
-    g_object_get(toy_text, "text", &text, NULL);
-    g_assert_cmpstr(text, ==, latin1_text);
-    g_free(text);
+    g_object_set(text, "text", latin1_text, NULL);
+    g_object_get(text, "text", &string, NULL);
+    g_assert_cmpstr(string, ==, latin1_text);
+    g_free(string);
 
-    g_object_set(toy_text, "text", NULL, NULL);
-    g_object_get(toy_text, "text", &text, NULL);
-    g_assert(text == NULL);
+    g_object_set(text, "text", NULL, NULL);
+    g_object_get(text, "text", &string, NULL);
+    g_assert(string == NULL);
 
-    adg_entity_destroy(ADG_ENTITY(toy_text));
+    adg_entity_destroy(ADG_ENTITY(text));
 }
 
 
@@ -117,8 +117,8 @@ main(int argc, char *argv[])
 {
     adg_test_init(&argc, &argv);
 
-    adg_test_add_func("/adg/text/font-dress", _adg_test_font_dress);
-    adg_test_add_func("/adg/text/text", _adg_test_text);
+    adg_test_add_func("/adg/string/font-dress", _adg_test_font_dress);
+    adg_test_add_func("/adg/string/string", _adg_test_text);
 
     return g_test_run();
 }
