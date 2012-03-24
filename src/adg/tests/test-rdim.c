a14386ff1b05bccf4fef5ea3bb99c8be32a6e161
Nicola Fontana <ntd@entidi.it>
2012-03-24 01:05:28 +0100

    tests: added test-rdim

---
+++ src/adg/tests/test-rdim.c
@@ -0,0 +1,46 @@
+/* ADG - Automatic Drawing Generation
+ * Copyright (C) 2010,2011,2012  Nicola Fontana <ntd at entidi.it>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301, USA.
+ */
+
+
+#include "test-internal.h"
+
+
+static void
+_adg_test_value(void)
+{
+    AdgDim *dim;
+    const gchar *value;
+
+    dim = ADG_DIM(adg_rdim_new());
+
+    /* Check the default value (other options are checked by test-dim */
+    value = adg_dim_get_value(dim);
+    g_assert_cmpstr(value, ==, "R <>");
+}
+
+
+int
+main(int argc, char *argv[])
+{
+    adg_test_init(&argc, &argv);
+
+    adg_test_add_func("/adg/rdim/property/value", _adg_test_value);
+
+    return g_test_run();
+}
