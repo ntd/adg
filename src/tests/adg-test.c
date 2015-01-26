90920c69d5f44e54a87f8fdfac2230989c90f0c4
Nicola Fontana <ntd@entidi.it>
2015-01-26 22:45:42 +0100

    tests: trying to reduce hacks
    Simplified the common test code to follow the standard building
    convention, i.e. "make V=1" will display warnings while "make V=0" will
    produce a clean list.

--- src/tests/adg-test.c
+++ src/tests/adg-test.c
@@ -22,6 +22,21 @@
 #include "adg-test.h"
 
 
+typedef struct {
+    void (*func)(gpointer);
+    gpointer data;
+} _TestData;
+
+
+/* Using adg_nop() would require to pull in the whole libadg library:
+ * better to replicate that trivial function instead.
+ */
+static void
+_adg_nop(void)
+{
+}
+
+
 void
 adg_test_init(int *p_argc, char **p_argv[])
 {
@@ -31,7 +46,16 @@ adg_test_init(int *p_argc, char **p_argv[])
     g_type_init();
 #endif
     g_test_init(p_argc, p_argv, NULL);
+
     g_log_set_always_fatal(0);
+
+    /* When building in silent mode (default), the ADG_QUIET
+     * environment variable is set to 1 by the Makefile and the
+     * warnings are discarded to reduce visual cluttering.
+     */
+    if (g_getenv("ADG_QUIET") != NULL)
+        g_log_set_default_handler((GLogFunc) _adg_nop, NULL);
+
     g_test_bug_base("http://dev.entidi.com/p/adg/issues/%s/");
 }
 
@@ -42,117 +66,12 @@ adg_test_invalid_pointer(void)
     return junk;
 }
 
-
-#if GLIB_CHECK_VERSION(2, 38, 0)
-
-/* Instead of using the deprecated fork approach (it does not always work)
- * use the newly introduced g_test_set_nonfatal_assertions() API to avoid
- * aborting the program and check the result at the end of the test.
- */
-
-typedef struct {
-    GSList *errors;
-    GLogFunc old_logger;
-} _AdgFixture;
-
-
-typedef struct {
-    gchar *log_domain;
-    GLogLevelFlags log_level;
-    gchar *message;
-} _AdgCapture;
-
-static void
-_adg_capture_handler(const gchar *log_domain, GLogLevelFlags log_level,
-                     const gchar *message, _AdgFixture *fixture)
-{
-    _AdgCapture *capture = g_new(_AdgCapture, 1);
-    capture->log_domain = g_strdup(log_domain);
-    capture->log_level = log_level;
-    capture->message = g_strdup(message);
-    fixture->errors = g_slist_append(fixture->errors, capture);
-}
-
-static void
-_adg_capture_free(_AdgCapture *capture)
-{
-    g_free(capture->log_domain);
-    g_free(capture->message);
-    g_free(capture);
-}
-
-static void
-_adg_setup(_AdgFixture *fixture, gconstpointer user_data)
-{
-    fixture->errors = NULL;
-    fixture->old_logger = g_log_set_default_handler((GLogFunc) _adg_capture_handler, fixture);
-}
-
-static void
-_adg_teardown(_AdgFixture *fixture, gconstpointer user_data)
-{
-    g_log_set_default_handler(fixture->old_logger, NULL);
-
-    if (g_test_failed()) {
-        GSList *node;
-        _AdgCapture *capture;
-
-        for (node = fixture->errors; node != NULL; node = node->next) {
-            capture = node->data;
-            g_log(capture->log_domain, capture->log_level,
-                  "%s", capture->message);
-        }
-    }
-
-    g_slist_free_full(fixture->errors, (GDestroyNotify) _adg_capture_free);
-}
-
-void
-adg_test_add_func_full(const char *testpath,
-                       GCallback test_func, gpointer user_data)
-{
-    g_test_set_nonfatal_assertions();
-    g_test_add(testpath, _AdgFixture, user_data,
-               _adg_setup, (gpointer) test_func, _adg_teardown);
-}
-
-#else
-
-#include <stdlib.h>
-
-typedef struct {
-    void (*func)(gpointer);
-    gpointer data;
-} _TestData;
-
-/* Using adg_nop() would require to pull in the whole libadg library:
- * better to replicate that trivial function instead.
- */
-static void
-_adg_nop(void)
-{
-}
-
 static void
 _adg_test_func(_TestData *test_data)
 {
-    GLogFunc old_logger;
-
-    /* Run a test in a forked environment, without showing log messages */
-    old_logger = g_log_set_default_handler((GLogFunc) _adg_nop, NULL);
-    if (g_test_trap_fork(0, G_TEST_TRAP_SILENCE_STDERR)) {
-        test_data->func(test_data->data);
-        exit(0);
-    }
-    g_log_set_default_handler(old_logger, NULL);
-
-    /* On failed test, rerun it without hiding the log messages */
-    if (!g_test_trap_has_passed())
-        test_data->func(test_data->data);
-
+    test_data->func(test_data->data);
     g_free(test_data);
 }
-
 
 void
 adg_test_add_func_full(const char *testpath,
@@ -164,8 +83,6 @@ adg_test_add_func_full(const char *testpath,
 
     g_test_add_data_func(testpath, test_data, (GTestDataFunc) _adg_test_func);
 }
-
-#endif
 
 void
 adg_test_add_func(const char *testpath, GCallback test_func)
