2c7ad02da20d1bf2e3dd4f407d10fd46794019e4
Nicola Fontana <ntd@entidi.it>
2015-01-29 19:04:13 +0100

    tests: increase coverage on CpmlPrimitive

--- src/cpml/tests/test-primitive.c
+++ src/cpml/tests/test-primitive.c
@@ -54,37 +54,27 @@ cairo_path_t path = {
 
 
 static void
-_cpml_test_basic(void)
+_cpml_test_from_segment(void)
 {
-    cairo_bool_t found;
     CpmlSegment segment;
-    CpmlPrimitive primitive, primitive_copy;
+    CpmlPrimitive primitive;
 
-    found = cpml_segment_from_cairo(&segment, &path);
-    g_assert_true(found);
+    g_assert_true(cpml_segment_from_cairo(&segment, &path));
 
-    /* Browsing API */
     cpml_primitive_from_segment(&primitive, &segment);
+    g_assert_nonnull(primitive.segment);
+    g_assert_nonnull(primitive.org);
     g_assert_nonnull(primitive.data);
-    found = cpml_primitive_next(&primitive);
-    g_assert_true(found);
-    found = cpml_primitive_next(&primitive);
-    g_assert_true(found);
-    found = cpml_primitive_next(&primitive);
-    g_assert_true(found);
 
-    /* Checking primitive copy and corner case browsing */
-    cpml_primitive_copy(&primitive_copy, &primitive);
-    found = cpml_primitive_next(&primitive);
-    g_assert_false(found);
-    cpml_primitive_reset(&primitive);
+    cpml_primitive_from_segment(&primitive, NULL);
+    g_assert_null(primitive.segment);
+    g_assert_null(primitive.org);
+    g_assert_null(primitive.data);
+
+    cpml_primitive_from_segment(&primitive, &segment);
+    g_assert_nonnull(primitive.segment);
+    g_assert_nonnull(primitive.org);
     g_assert_nonnull(primitive.data);
-    g_assert_cmpint((primitive.data)->header.type, ==, CPML_LINE);
-    found = cpml_primitive_next(&primitive_copy);
-    g_assert_false(found);
-    cpml_primitive_reset(&primitive_copy);
-    g_assert_nonnull(primitive_copy.data);
-    g_assert_cmpint((primitive_copy.data)->header.type, ==, CPML_LINE);
 }
 
 static void
@@ -340,17 +330,264 @@ _cpml_test_put_point(void)
     g_assert_cmpfloat(pair.y, ==, 1);
 }
 
+static void
+_cpml_test_get_length(void)
+{
+    cairo_path_data_t rectangle_data[] = {
+        { .header = { CPML_MOVE, 2 }},
+        { .point = { 0, 0 }},
+
+        { .header = { CPML_LINE, 2 }},
+        { .point = { 2, 0 }},
+
+        { .header = { CPML_LINE, 2 }},
+        { .point = { 2, 1 }},
+
+        { .header = { CPML_LINE, 2 }},
+        { .point = { 0, 1 }},
+
+        { .header = { CPML_CLOSE, 1 }}
+    };
+    cairo_path_t rectangle = {
+        CAIRO_STATUS_SUCCESS,
+        rectangle_data,
+        G_N_ELEMENTS(rectangle_data)
+    };
+    CpmlSegment segment;
+    CpmlPrimitive primitive;
+
+    g_assert_cmpfloat(cpml_primitive_get_length(NULL), ==, 0);
+
+    cpml_segment_from_cairo(&segment, &rectangle);
+
+    cpml_primitive_from_segment(&primitive, &segment);
+    g_assert_cmpfloat(cpml_primitive_get_length(&primitive), ==, 2);
+
+    cpml_primitive_next(&primitive);
+    g_assert_cmpfloat(cpml_primitive_get_length(&primitive), ==, 1);
+
+    cpml_primitive_next(&primitive);
+    g_assert_cmpfloat(cpml_primitive_get_length(&primitive), ==, 2);
+
+    cpml_primitive_next(&primitive);
+    g_assert_cmpfloat(cpml_primitive_get_length(&primitive), ==, 1);
+}
+
+static void
+_cpml_test_put_intersections(void)
+{
+    cairo_path_data_t path1_data[] = {
+        { .header = { CPML_MOVE, 2 }},
+        { .point = { -2, 1 }},
+
+        { .header = { CPML_LINE, 2 }},
+        { .point = { 2, 1 }},
+
+        { .header = { CPML_LINE, 2 }},
+        { .point = { 2, 0 }},
+
+        { .header = { CPML_LINE, 2 }},
+        { .point = { -2, 0 }}
+    }, path2_data[] = {
+        { .header = { CPML_MOVE, 2 }},
+        { .point = { 0, -1 }},
+
+        { .header = { CPML_LINE, 2 }},
+        { .point = { 0, 2 }}
+    };
+    cairo_path_t path1 = {
+        CAIRO_STATUS_SUCCESS,
+        path1_data,
+        G_N_ELEMENTS(path1_data)
+    }, path2 = {
+        CAIRO_STATUS_SUCCESS,
+        path2_data,
+        G_N_ELEMENTS(path2_data)
+    };
+    CpmlSegment segment;
+    CpmlPrimitive primitive, primitive2;
+    CpmlPair pair[2];
+
+    cpml_segment_from_cairo(&segment, &path2);
+    cpml_primitive_from_segment(&primitive, &segment);
+
+    cpml_segment_from_cairo(&segment, &path1);
+    cpml_primitive_from_segment(&primitive2, &segment);
+
+    g_assert_cmpint(cpml_primitive_put_intersections(NULL, &primitive2, 2, pair), ==, 0);
+    g_assert_cmpint(cpml_primitive_put_intersections(&primitive, NULL, 2, pair), ==, 0);
+    g_assert_cmpint(cpml_primitive_put_intersections(NULL, NULL, 2, pair), ==, 0);
+    g_assert_cmpint(cpml_primitive_put_intersections(&primitive, &primitive2, 0, pair), ==, 0);
+    g_assert_cmpint(cpml_primitive_put_intersections(&primitive, &primitive2, 2, NULL), ==, 0);
+
+    /* The first primitive of path1 intersects path2 in (0, 1) */
+    g_assert_cmpint(cpml_primitive_put_intersections(&primitive, &primitive2, 2, pair), ==, 1);
+    g_assert_cmpfloat(pair[0].x, ==, 0);
+    g_assert_cmpfloat(pair[0].y, ==, 1);
+
+    /* The second primitive does not intersect */
+    cpml_primitive_next(&primitive2);
+    g_assert_cmpint(cpml_primitive_put_intersections(&primitive, &primitive2, 2, pair), ==, 0);
+
+    /* The third primitive intersects in (0, 0) */
+    cpml_primitive_next(&primitive2);
+    g_assert_cmpint(cpml_primitive_put_intersections(&primitive, &primitive2, 2, pair), ==, 1);
+    g_assert_cmpfloat(pair[0].x, ==, 0);
+    g_assert_cmpfloat(pair[0].y, ==, 0);
+
+    g_assert_cmpint(cpml_primitive_put_intersections_with_segment(NULL, &segment, 2, pair), ==, 0);
+    g_assert_cmpint(cpml_primitive_put_intersections_with_segment(&primitive, NULL, 2, pair), ==, 0);
+    g_assert_cmpint(cpml_primitive_put_intersections_with_segment(NULL, NULL, 2, pair), ==, 0);
+    g_assert_cmpint(cpml_primitive_put_intersections_with_segment(&primitive, &segment, 0, pair), ==, 0);
+    g_assert_cmpint(cpml_primitive_put_intersections_with_segment(&primitive, &segment, 2, NULL), ==, 0);
+
+    /* path2 intesects path1 in two points */
+    g_assert_cmpint(cpml_primitive_put_intersections_with_segment(&primitive, &segment, 2, pair), ==, 2);
+    g_assert_cmpfloat(pair[0].x, ==, 0);
+    g_assert_cmpfloat(pair[0].y, ==, 1);
+    g_assert_cmpfloat(pair[1].x, ==, 0);
+    g_assert_cmpfloat(pair[1].y, ==, 0);
+}
+
+static void
+_cpml_test_join(void)
+{
+    cairo_path_data_t path_data[] = {
+        { .header = { CPML_MOVE, 2 }},
+        { .point = { 0, 0 }},
+
+        { .header = { CPML_LINE, 2 }},
+        { .point = { 2, 0 }},
+
+        { .header = { CPML_LINE, 2 }},
+        { .point = { 2, 2 }},
+
+        { .header = { CPML_LINE, 2 }},
+        { .point = { 1, 2 }},
+
+        { .header = { CPML_LINE, 2 }},
+        { .point = { 1, -2 }}
+    };
+    cairo_path_t path = {
+        CAIRO_STATUS_SUCCESS,
+        path_data,
+        G_N_ELEMENTS(path_data)
+    };
+    CpmlSegment segment;
+    CpmlPrimitive primitive1, primitive2;
+
+    cpml_segment_from_cairo(&segment, &path);
+
+    cpml_primitive_from_segment(&primitive1, &segment);
+    cpml_primitive_copy(&primitive2, &primitive1);
+    cpml_primitive_next(&primitive2);
+
+    /* primitive1 and primitive2 are already joint */
+    g_assert_cmpint(cpml_primitive_join(&primitive1, &primitive2), ==, 1);
+    g_assert_cmpfloat((primitive2.org)->point.x, ==, 2);
+    g_assert_cmpfloat((primitive2.org)->point.y, ==, 0);
+
+    cpml_primitive_next(&primitive2);
+    /* Now primitive1 and primitive2 are divergent,
+     * hence cannot be joined */
+    g_assert_cmpint(cpml_primitive_join(&primitive1, &primitive2), ==, 0);
+
+    cpml_primitive_next(&primitive2);
+    g_assert_cmpint(cpml_primitive_join(&primitive1, &primitive2), ==, 1);
+    g_assert_cmpfloat((primitive2.org)->point.x, ==, 1);
+    g_assert_cmpfloat((primitive2.org)->point.y, ==, 0);
+}
+
+static void
+_cpml_test_dump(void)
+{
+#if GLIB_CHECK_VERSION(2, 38, 0)
+    if (g_test_subprocess()) {
+        CpmlSegment segment;
+        CpmlPrimitive primitive;
+
+        /* This should not crash the process */
+        cpml_primitive_dump(NULL, 1);
+
+        cpml_segment_from_cairo(&segment, &path);
+        cpml_primitive_from_segment(&primitive, &segment);
+        cpml_primitive_dump(&primitive, 1);
+
+        cpml_primitive_next(&primitive);
+        cpml_primitive_dump(&primitive, 1);
+
+        return;
+    }
+
+    g_test_trap_subprocess(NULL, 0, 0);
+    g_test_trap_assert_passed();
+    g_test_trap_assert_stderr_unmatched("?");
+    g_test_trap_assert_stdout("*NULL*");
+    g_test_trap_assert_stdout("*move*");
+    g_test_trap_assert_stdout("*line*");
+#endif
+}
+
+static void
+_cpml_test_browsing(void)
+{
+    CpmlSegment segment;
+    CpmlPrimitive primitive, primitive_copy;
+
+    cpml_segment_from_cairo(&segment, &path);
+
+    cpml_primitive_from_segment(&primitive, &segment);
+    g_assert_cmpfloat((primitive.org)->point.x, ==, 0);
+    g_assert_cmpfloat((primitive.org)->point.y, ==, 1);
+    g_assert_cmpint((primitive.data)->header.type, ==, CPML_LINE);
+    g_assert_true(cpml_primitive_next(&primitive));
+    g_assert_cmpfloat((primitive.org)->point.x, ==, 2);
+    g_assert_cmpfloat((primitive.org)->point.y, ==, 3);
+    g_assert_cmpint((primitive.data)->header.type, ==, CPML_ARC);
+    g_assert_true(cpml_primitive_next(&primitive));
+    g_assert_cmpfloat((primitive.org)->point.x, ==, 6);
+    g_assert_cmpfloat((primitive.org)->point.y, ==, 7);
+    g_assert_cmpint((primitive.data)->header.type, ==, CPML_CURVE);
+    g_assert_true(cpml_primitive_next(&primitive));
+    g_assert_cmpfloat((primitive.org)->point.x, ==, 12);
+    g_assert_cmpfloat((primitive.org)->point.y, ==, 13);
+    g_assert_cmpint((primitive.data)->header.type, ==, CPML_CLOSE);
+    g_assert_false(cpml_primitive_next(&primitive));
+
+    cpml_primitive_reset(&primitive);
+    g_assert_true(cpml_primitive_next(&primitive));
+    cpml_primitive_reset(&primitive);
+    cpml_primitive_reset(&primitive);
+    g_assert_true(cpml_primitive_next(&primitive));
+    g_assert_true(cpml_primitive_next(&primitive));
+    g_assert_true(cpml_primitive_next(&primitive));
+    g_assert_false(cpml_primitive_next(&primitive));
+
+    cpml_primitive_copy(&primitive_copy, &primitive);
+    g_assert_false(cpml_primitive_next(&primitive_copy));
+    cpml_primitive_reset(&primitive);
+    g_assert_false(cpml_primitive_next(&primitive_copy));
+    cpml_primitive_reset(&primitive_copy);
+    g_assert_true(cpml_primitive_next(&primitive_copy));
+}
+
 
 int
 main(int argc, char *argv[])
 {
     adg_test_init(&argc, &argv);
 
-    adg_test_add_func("/cpml/primitive/basic", _cpml_test_basic);
-    adg_test_add_func("/cpml/primitive/type_get_n_points", _cpml_test_type_get_n_points);
-    adg_test_add_func("/cpml/primitive/get_n_points", _cpml_test_get_n_points);
-    adg_test_add_func("/cpml/primitive/set_point", _cpml_test_set_point);
-    adg_test_add_func("/cpml/primitive/put_point", _cpml_test_put_point);
+    adg_test_add_func("/cpml/primitive/method/from-segment", _cpml_test_from_segment);
+    adg_test_add_func("/cpml/primitive/method/type-get-n-points", _cpml_test_type_get_n_points);
+    adg_test_add_func("/cpml/primitive/method/get-n-points", _cpml_test_get_n_points);
+    adg_test_add_func("/cpml/primitive/method/set-point", _cpml_test_set_point);
+    adg_test_add_func("/cpml/primitive/method/put-point", _cpml_test_put_point);
+    adg_test_add_func("/cpml/primitive/method/get-length", _cpml_test_get_length);
+    adg_test_add_func("/cpml/primitive/method/put-intersections", _cpml_test_put_intersections);
+    adg_test_add_func("/cpml/primitive/method/join", _cpml_test_join);
+    adg_test_add_func("/cpml/primitive/method/dump", _cpml_test_dump);
+
+    adg_test_add_func("/cpml/primitive/behavior/browsing", _cpml_test_browsing);
 
     return g_test_run();
 }
