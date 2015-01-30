9c0c5a3c7704060cf441d685359362758dfe27a2
Nicola Fontana <ntd@entidi.it>
2015-01-30 15:46:39 +0100

    tests: complete cpml-gobject coverage

--- src/cpml/tests/test-gobject.c
+++ src/cpml/tests/test-gobject.c
@@ -22,14 +22,176 @@
 #include <cpml.h>
 
 
+static cairo_path_data_t data[] = {
+    { .header = { CPML_MOVE, 2 } },
+    { .point = { 1, 2 } },
+    { .header = { CPML_LINE, 2 } },
+    { .point = { 3, 4 } }
+};
+static cairo_path_t path = {
+    CAIRO_STATUS_SUCCESS,
+    data,
+    G_N_ELEMENTS(data)
+};
+
+
+static void
+_cpml_test_primitive_deep_dup(void)
+{
+    CpmlSegment segment = { &path, data, G_N_ELEMENTS(data) };
+    CpmlPrimitive primitive, *dup_primitive;
+
+    g_assert_null(cpml_primitive_deep_dup(NULL));
+
+    /* Test full duplication */
+    primitive.segment = &segment;
+    primitive.org = &data[1];
+    primitive.data = &data[2];
+
+    dup_primitive = cpml_primitive_deep_dup(&primitive);
+    g_assert_nonnull(dup_primitive);
+    g_assert_nonnull(dup_primitive->segment);
+    g_assert_nonnull(dup_primitive->org);
+    g_assert_nonnull(dup_primitive->data);
+    g_assert_false(dup_primitive->segment == primitive.segment);
+    g_assert_false(dup_primitive->org == primitive.org);
+    g_assert_false(dup_primitive->data == primitive.data);
+    g_free(dup_primitive);
+
+    /* Test duplication with NULL fields */
+    primitive.segment = NULL;
+
+    dup_primitive = cpml_primitive_deep_dup(&primitive);
+    g_assert_nonnull(dup_primitive);
+    g_assert_null(dup_primitive->segment);
+    g_assert_nonnull(dup_primitive->org);
+    g_assert_nonnull(dup_primitive->data);
+    g_free(dup_primitive);
+
+    primitive.org = NULL;
+
+    dup_primitive = cpml_primitive_deep_dup(&primitive);
+    g_assert_nonnull(dup_primitive);
+    g_assert_null(dup_primitive->segment);
+    g_assert_null(dup_primitive->org);
+    g_assert_nonnull(dup_primitive->data);
+    g_free(dup_primitive);
+
+    primitive.data = NULL;
+
+    dup_primitive = cpml_primitive_deep_dup(&primitive);
+    g_assert_nonnull(dup_primitive);
+    g_assert_null(dup_primitive->segment);
+    g_assert_null(dup_primitive->org);
+    g_assert_null(dup_primitive->data);
+    g_free(dup_primitive);
+}
+
+static void
+_cpml_test_segment_deep_dup(void)
+{
+    CpmlSegment segment, *dup_segment;
+
+    g_assert_null(cpml_segment_deep_dup(NULL));
+
+    /* Test full duplication */
+    segment.path = &path;
+    segment.data = data;
+    segment.num_data = G_N_ELEMENTS(data);
+
+    dup_segment = cpml_segment_deep_dup(&segment);
+    g_assert_nonnull(dup_segment);
+    g_assert_null(dup_segment->path);
+    g_assert_nonnull(dup_segment->data);
+    g_assert_false(dup_segment->data == segment.data);
+    g_assert_cmpint(dup_segment->num_data, ==, segment.num_data);
+    g_free(dup_segment);
+
+    /* Test duplication with NULL fields */
+    segment.path = NULL;
+
+    dup_segment = cpml_segment_deep_dup(&segment);
+    g_assert_nonnull(dup_segment);
+    g_assert_null(dup_segment->path);
+    g_assert_nonnull(dup_segment->data);
+    g_assert_false(dup_segment->data == segment.data);
+    g_assert_cmpint(dup_segment->num_data, ==, segment.num_data);
+    g_free(dup_segment);
+
+    segment.data = NULL;
+
+    dup_segment = cpml_segment_deep_dup(&segment);
+    g_assert_nonnull(dup_segment);
+    g_assert_cmpint(dup_segment->num_data, ==, 0);
+    g_assert_null(dup_segment->data);
+    g_free(dup_segment);
+
+    segment.data = data;
+    segment.num_data = 0;
+
+    dup_segment = cpml_segment_deep_dup(&segment);
+    g_assert_nonnull(dup_segment);
+    g_assert_cmpint(dup_segment->num_data, ==, 0);
+    g_assert_null(dup_segment->data);
+    g_free(dup_segment);
+}
+
+static void
+_cpml_test_segment_deep_copy(void)
+{
+    CpmlSegment original, *segment, *dup_segment;
+
+    original.path = &path;
+    original.data = data;
+    original.num_data = G_N_ELEMENTS(data);
+
+    /* Check it does not crash with NULL values */
+    cpml_segment_deep_copy(NULL, NULL);
+    cpml_segment_deep_copy(NULL, &original);
+    cpml_segment_deep_copy(&original, NULL);
+
+    /* Test full deep copy */
+    segment = cpml_segment_deep_dup(&original);
+    dup_segment = cpml_segment_deep_dup(segment);
+    dup_segment->data[0].point.x = 123;
+    dup_segment->data[0].point.y = 456;
+    dup_segment->data[2].point.x = 789;
+    dup_segment->data[2].point.y = 101;
+    cpml_segment_deep_copy(segment, dup_segment);
+    g_assert_cmpfloat(segment->data[0].point.x, ==, 123);
+    g_assert_cmpfloat(segment->data[0].point.y, ==, 456);
+    g_assert_cmpfloat(segment->data[2].point.x, ==, 789);
+    g_assert_cmpfloat(segment->data[2].point.y, ==, 101);
+    g_free(dup_segment);
+    g_free(segment);
+
+    /* Check segments with different num_data are not copied */
+    segment = cpml_segment_deep_dup(&original);
+    g_assert_cmpfloat(segment->data[0].point.x, !=, 123);
+    dup_segment = cpml_segment_deep_dup(segment);
+    dup_segment->num_data = 12;
+    dup_segment->data[0].point.x = 123;
+    cpml_segment_deep_copy(segment, dup_segment);
+    g_assert_cmpfloat(segment->data[0].point.x, !=, 123);
+    g_free(dup_segment);
+    g_free(segment);
+}
+
+
 int
 main(int argc, char *argv[])
 {
     adg_test_init(&argc, &argv);
 
     adg_test_add_boxed_checks("/cpml/pair/type/boxed", CPML_TYPE_PAIR, g_new0(CpmlPair, 1));
+
     adg_test_add_boxed_checks("/cpml/primitive/type/boxed", CPML_TYPE_PRIMITIVE, g_new0(CpmlPrimitive, 1));
+    adg_test_add_func("/cpml/primitive/method/deep-dup", _cpml_test_primitive_deep_dup);
+
     adg_test_add_boxed_checks("/cpml/segment/type/boxed", CPML_TYPE_SEGMENT, g_new0(CpmlSegment, 1));
+    adg_test_add_func("/cpml/segment/method/deep-dup", _cpml_test_segment_deep_dup);
+    adg_test_add_func("/cpml/segment/method/deep-copy", _cpml_test_segment_deep_copy);
+
     adg_test_add_enum_checks("/cpml/curve-offset-algorithm/type/enum", CPML_TYPE_CURVE_OFFSET_ALGORITHM);
 
     return g_test_run();
