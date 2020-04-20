cf8f24a45549e0df8c24a9f1c8027eedd6a9e837
Nicola Fontana <ntd@entidi.it>
2020-04-20 09:34:21 +0200

    doc: update copyright line for 2020

--- src/cpml/cpml-primitive-private.h
+++ src/cpml/cpml-primitive-private.h
@@ -1,5 +1,5 @@
 /* CPML - Cairo Path Manipulation Library
- * Copyright (C) 2007-2019  Nicola Fontana <ntd at entidi.it>
+ * Copyright (C) 2007-2020  Nicola Fontana <ntd at entidi.it>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
